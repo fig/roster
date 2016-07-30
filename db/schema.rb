b892f1b89bd701020c23e594190bed48afe88db1
Phil <git@thebeach.plus.com>
2016-07-30 23:32:28 +0100

    linting

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160728214840) do
+ActiveRecord::Schema.define(version: 20160730015223) do
+
+  create_table "lines", force: :cascade do |t|
+    t.string   "number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "turns", force: :cascade do |t|
     t.string   "name"
