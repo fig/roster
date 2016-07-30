166a5e36817aa45f20c4fbad7d22adaf0fe4835b
Phil <git@thebeach.plus.com>
2016-07-30 23:51:48 +0100

    hmm

---
+++ test/models/line_test.rb
@@ -0,0 +1,12 @@
+require 'test_helper'
+
+class LineTest < ActiveSupport::TestCase
+  def setup
+    @line = Line.new number: '01'
+  end
+
+  test 'should not save line without number' do
+    @line = Line.new
+    assert_not line.save, 'Saved the line without a name'
+  end
+end
